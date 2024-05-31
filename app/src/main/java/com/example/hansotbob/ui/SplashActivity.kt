1ef2f29770f34063ae0e027e0433c850154ad345
Cycrypto <jh01love00@gmail.com>
2024-05-31 11:03:44 +0900

    update: change some splashview settigns

--- app/src/main/java/com/example/hansotbob/ui/SplashActivity.kt
+++ app/src/main/java/com/example/hansotbob/ui/SplashActivity.kt
@@ -10,24 +10,23 @@ import com.viksaa.sssplash.lib.model.ConfigSplash
 
 
 class SplashView : AwesomeSplash() {
-
     override fun initSplash(configSplash: ConfigSplash) {
         // Customize Circular Reveal
-        configSplash.backgroundColor = R.color.pink // any color you want from colors.xml
+        configSplash.backgroundColor = R.color.yellow // any color you want from colors.xml
         configSplash.animCircularRevealDuration = 2000 // int ms
         configSplash.revealFlagX = Flags.REVEAL_RIGHT // or Flags.REVEAL_LEFT
         configSplash.revealFlagY = Flags.REVEAL_BOTTOM // or Flags.REVEAL_TOP
 
         // Customize Logo
         configSplash.logoSplash = R.drawable.logo // or any other drawable
-        configSplash.animLogoSplashDuration = 2000 // int ms
+        configSplash.animLogoSplashDuration = 1800 // int ms
         configSplash.animLogoSplashTechnique = Techniques.FadeIn // choose one from Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
 
         // Customize Title
         configSplash.titleSplash = "한솥밥"
         configSplash.titleTextColor = R.color.white
-        configSplash.titleTextSize = 30f // float value
-        configSplash.animTitleDuration = 3000
+        configSplash.titleTextSize = 50f // float value
+        configSplash.animTitleDuration = 2000
         configSplash.animTitleTechnique = Techniques.FadeIn
         configSplash.titleFont = "font/WandohopeB.ttf" // provide a font in assets/fonts
     }
