87cd3330d62ebdff5713046230c9616ac5a91ef9
Cycrypto <jh01love00@gmail.com>
2024-06-05 14:00:34 +0900

    feat : connect onboarding activity to main activity

--- app/src/main/java/com/example/hansotbob/ui/OnboardingActivity.kt
+++ app/src/main/java/com/example/hansotbob/ui/OnboardingActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hansotbob.ui
 
 import android.animation.Animator
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
@@ -14,58 +15,65 @@ import com.ramotion.paperonboarding.PaperOnboardingFragment
 import com.ramotion.paperonboarding.PaperOnboardingPage
 import com.ramotion.paperonboarding.listeners.PaperOnboardingOnChangeListener
 import com.ramotion.paperonboarding.listeners.PaperOnboardingOnRightOutListener
-
 class OnboardingActivity : AppCompatActivity() {
 
-    private lateinit var container: FrameLayout
-    private lateinit var revealView: View
-    private var currentFragment: PaperOnboardingFragment? = null
+        private lateinit var container: FrameLayout
+        private lateinit var revealView: View
+        private var currentFragment: PaperOnboardingFragment? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_onboarding)
 
-        container = findViewById(R.id.container)
-        revealView = findViewById(R.id.reveal_view)
+            container = findViewById(R.id.container)
+            revealView = findViewById(R.id.reveal_view)
 
-        val page1 = PaperOnboardingPage(
-            "집밥 공유",
-            "지역 공동체 사람들과 집밥을 공유해보세요",
-            Color.parseColor("#FFFFFF"),
-            R.drawable.sharing_meal_resized,
-            R.drawable.ic_rice
-        )
+            val page1 = PaperOnboardingPage(
+                "집밥 공유",
+                "지역 공동체 사람들과 집밥을 공유해보세요",
+                Color.parseColor("#FFFFFF"),
+                R.drawable.img_share_meal,
+                R.drawable.ic_rice
+            )
 
-        val page2 = PaperOnboardingPage(
-            "재료 공유",
-            "혼자 소비하기 많은 재료들을 공동으로 구매 또는 나눠보세요.",
-            Color.parseColor("#FFFFFF"),
-            R.drawable.sharing_ingredient_resized,
-            R.drawable.ic_rice
-        )
+            val page2 = PaperOnboardingPage(
+                "재료 공유",
+                "혼자 소비하기 많은 재료들을 공동으로 구매 또는 나눠보세요.",
+                Color.parseColor("#FFFFFF"),
+                R.drawable.img_share_ingredient,
+                R.drawable.ic_rice
+            )
 
-        val page3 = PaperOnboardingPage(
-            "레시피 공유",
-            "나만의 레시피를 공동체와 함께 공유해보세요.",
-            Color.parseColor("#FFFFFF"),
-            R.drawable.sharing_recipe_resized,
-            R.drawable.ic_next_arrow
-        )
+            val page3 = PaperOnboardingPage(
+                "레시피 공유",
+                "나만의 레시피를 공동체와 함께 공유해보세요.",
+                Color.parseColor("#FFFFFF"),
+                R.drawable.img_share_recipies,
+                R.drawable.ic_next_arrow
+            )
 
-        val elements = arrayListOf(page1, page2, page3)
+            val elements = arrayListOf(page1, page2, page3)
 
-        val onBoardingFragment = PaperOnboardingFragment.newInstance(elements)
-        currentFragment = onBoardingFragment
+            val onBoardingFragment = PaperOnboardingFragment.newInstance(elements)
+            currentFragment = onBoardingFragment
 
 //        onBoardingFragment.setOnChangeListener(PaperOnboardingOnChangeListener { position, _ ->
 //            val color = Color.parseColor("#FFA726")
 //            performCircularReveal(onBoardingFragment, color)
 //        })
 
-        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.container, onBoardingFragment)
-        fragmentTransaction.commit()
-    }
+            onBoardingFragment.setOnRightOutListener {
+                // When the user swipes out from the last page, start MainActivity
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Finish the onboarding activity so it can't be returned to
+            }
+
+            val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.add(R.id.container, onBoardingFragment)
+            fragmentTransaction.commit()
+        }
+}
 
 // 에니메이션 관련 주석 (필요시 사용)
 
@@ -104,4 +112,3 @@ class OnboardingActivity : AppCompatActivity() {
 //            override fun onAnimationRepeat(animation: Animator) {}
 //        })
 //    }
-}
