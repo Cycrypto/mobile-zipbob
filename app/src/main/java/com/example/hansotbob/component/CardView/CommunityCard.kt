617ecf6cc42cdcbea766b5e70d8c2972a5985d85
edgarlee1 <wns6001@naver.com>
2024-06-20 13:11:03 +0900

    중간설정

---
+++ app/src/main/java/com/example/hansotbob/component/CardView/CommunityCard.kt
@@ -0,0 +1,310 @@
+package com.example.hansotbob.component.CardView
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Badge
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.hansotbob.R
+import com.example.hansotbob.ui.theme.HansotbobTheme
+
+
+@Composable
+fun CommunityCard(
+    title: String,
+    recruit: String,
+    place: String,
+    price: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(10.dp)
+        ) {
+            Image(
+                painter = imagePainter,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(140.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.Black)
+                    .padding(end = 10.dp),
+                contentScale = ContentScale.Crop // 이미지가 꽉 차도록 설정
+            )
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(10.dp)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold),
+                        fontSize = 25.sp
+                    )
+                    Text(text = recruit)
+                    Text(text = place)
+                }
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(end = 10.dp, bottom = 10.dp)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = price,
+                            fontSize = 20.sp,
+                        )
+                        Text(
+                            text = "pts",
+                            fontSize = 20.sp,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun OverviewCard2(imageRes:Int, name:String, category:String, modifier:Modifier) {
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        modifier = modifier
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()  // Ensure the column fills the width of the card
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,  // Adjust image to fill the width
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()  // Ensure this column also fills the width
+                    .padding(16.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(name)
+                    Icon(
+                        Icons.Default.Favorite,
+                        contentDescription = "Favorites",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+                Text(category, style = MaterialTheme.typography.bodySmall)
+            }
+        }
+    }
+}
+
+@Composable
+fun CommunityCardWithBadge(
+    title: String,
+    date: String,
+    category: String,
+    points: String,
+    imagePainter: Painter,
+    isNew: Boolean = false,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Box {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(10.dp)
+            ) {
+                BadgeBox2(
+                    badge = {
+                        if (isNew) {
+                            Badge(
+                                modifier = Modifier
+                                    .offset(-2.dp, (-4).dp)
+                                    .size(20.dp)
+                                ,
+                                content = {
+                                    Text(
+                                        text = "N",
+                                        color = Color.White,
+                                        style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold)
+                                    )
+                                },
+                                containerColor = MaterialTheme.colorScheme.error
+                            )
+                        }
+                    }
+                ) {
+                    Image(
+                        painter = imagePainter,
+                        contentDescription = null,
+                        modifier = Modifier
+
+                            .width(140.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(MaterialTheme.colorScheme.onBackground),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(10.dp)
+                ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold),
+                        fontSize = 15.sp,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            imageVector = Icons.Default.DateRange,
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp),
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(
+                            text = date,
+                            style = MaterialTheme.typography.bodySmall,
+                            color = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = category,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.align(Alignment.Bottom)
+                ) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(CircleShape)
+                            .background(MaterialTheme.colorScheme.primary)
+                    ) {
+                        Text(
+                            text = points,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BadgeBox2(
+    badge: @Composable BoxScope.() -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable BoxScope.() -> Unit
+) {
+    Box(modifier = modifier) {
+        content()
+        badge()
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun PreviewCommunityCard() {
+    MealCategoryCard(
+        title = "제목",
+        recruit = "거래모집인원",
+        place = "거래장소",
+        price = "10000pt",
+        imagePainter = painterResource(id = R.drawable.food_image)
+    )
+}
+
+
+@Preview(showBackground = false)
+@Composable
+fun PreviewCommunityCard3() {
+    HansotbobTheme {
+        MealCategoryCardWithBadge(
+            title = "Hello",
+            date = "2023.12.21",
+            category = "양식",
+            points = "1000",
+            imagePainter = painterResource(id = R.drawable.food_image),
+            isNew = true
+        )
+    }
+
+}
+
