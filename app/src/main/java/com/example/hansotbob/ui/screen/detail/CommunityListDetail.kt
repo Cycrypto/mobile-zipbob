9de867fb3aa6e70fcfacd0e3e19fe7963aff7a45
edgarlee1 <wns6001@naver.com>
2024-06-21 15:11:56 +0900

    2-3-3구성을 위한 카드 생성및 적용

--- app/src/main/java/com/example/hansotbob/ui/screen/detail/CommunityListDetail.kt
+++ app/src/main/java/com/example/hansotbob/ui/screen/detail/CommunityListDetail.kt
@@ -1,27 +1,27 @@
-import androidx.compose.foundation.Image
+
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.hansotbob.R
 import com.example.hansotbob.ui.theme.HansotbobTheme
-
 
 
 @Composable
@@ -38,7 +38,7 @@ fun CommunityListDetail(navController: NavHostController, cards: List<CommunityC
                 .verticalScroll(rememberScrollState())
         ) {
             cards.forEach { card ->
-                CommunityCardWithBadge(
+                CommunityDetailCardWithBadge(
                     title = card.title,
                     imagePainter = card.imagePainter,
                     currentPeople = card.currentPeople,
